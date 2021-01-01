779a3f7d4eef1a49bf89dbfda92c2d04b0fb54df
rdx7777 <radek.jerzynski@gmail.com>
2021-01-01 23:20:18 +0000

    initial commit

---
+++ src/main/java/io/github/rdx7777/exercisesbackend/ExercisesBackendApplication.java
@@ -0,0 +1,12 @@
+package io.github.rdx7777.exercisesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/rdx7777/exercisesbackend/ExercisesBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.rdx7777.exercisesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
