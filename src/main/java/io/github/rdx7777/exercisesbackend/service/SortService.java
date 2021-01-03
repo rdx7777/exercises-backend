096a697951f5c8ef171f28b96bcb8b900c5bb2d5
rdx7777 <radek.jerzynski@gmail.com>
2021-01-03 00:55:17 +0000

    added SortServiceTest class and major changes in SortService

--- src/main/java/io/github/rdx7777/exercisesbackend/service/SortService.java
+++ src/main/java/io/github/rdx7777/exercisesbackend/service/SortService.java
@@ -8,82 +8,93 @@ import io.github.rdx7777.exercisesbackend.service.helpers.sort.SelectionSort;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
+import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SortService {
 
-    public List<SortedObject> getAllResults(Integer arraySize) {
-        List<SortedObject> results = new ArrayList<>();
+    private final Logger logger = LoggerFactory.getLogger(SortService.class);
+
+    Map<Long, Integer[]> results = new LinkedHashMap<>();
+
+    public Map<Long, Integer[]> getAllResults(Integer arraySize) {
+        if (arraySize == null) {
+            logger.error("Attempt to get results providing null array size.");
+            throw new IllegalArgumentException("Array size cannot be null.");
+        }
+        if (arraySize < 0) {
+            logger.error("Attempt to get results providing array size lower than 0.");
+            throw new IllegalArgumentException("Array size cannot be lower than zero.");
+        }
+        if (arraySize > 1_000_000) {
+            logger.error("Attempt to get results providing array size out of supported range.");
+            throw new IllegalArgumentException("Array size out of supported range. The maximum supported array size is 1,000,000.");
+        }
         int[] unsortedArray = createUnsortedArray(arraySize);
-        results.add(getBubbleSortResult(unsortedArray.clone()));
-        results.add(getCollectionSortResult(unsortedArray.clone()));
-        results.add(getMergeSortResult(unsortedArray.clone()));
-        results.add(getQuickSortResult(unsortedArray.clone()));
-        results.add(getSelectionSortResult(unsortedArray.clone()));
+        results.clear();
+        getBubbleSortResult(unsortedArray.clone());
+        getCollectionSortResult(unsortedArray.clone());
+        getMergeSortResult(unsortedArray.clone());
+        getQuickSortResult(unsortedArray.clone());
+        getSelectionSortResult(unsortedArray.clone());
         return results;
     }
 
-    private static class SortedObject {
-
-        private Long time;
-        private Integer[] sortedArray;
-
-        private SortedObject(Long time, Integer[] sortedArray) {
-            this.time = time;
-            this.sortedArray = sortedArray;
-        }
-    }
-
     private int[] createUnsortedArray(Integer arraySize) {
-        Random random = new Random();
-        int[] unsortedArray = new int[arraySize];
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < arraySize; i++) {
-            unsortedArray[i] = random.nextInt(arraySize);
+            numbers.add(i);
         }
+        Collections.shuffle(numbers);
+        int[] unsortedArray = IntStream.range(0, arraySize).map(numbers::get).toArray();
         return unsortedArray;
     }
 
-    private SortedObject getBubbleSortResult(int[] unsortedArray) {
+    private void getBubbleSortResult(int[] unsortedArray) {
         BubbleSort bubbleSort = new BubbleSort();
         long startTime = System.currentTimeMillis();
         int[] bubbleSortedArray = bubbleSort.sort(unsortedArray);
         Long bubbleSortedTime = System.currentTimeMillis() - startTime;
-        return new SortedObject(bubbleSortedTime, Arrays.stream(bubbleSortedArray).boxed().toArray(Integer[]::new));
+        results.put(bubbleSortedTime, Arrays.stream(bubbleSortedArray).boxed().toArray(Integer[]::new));
     }
 
-    private SortedObject getCollectionSortResult(int[] unsortedArray) {
+    private void getCollectionSortResult(int[] unsortedArray) {
         CollectionsSort collectionsSort = new CollectionsSort();
         long startTime = System.currentTimeMillis();
         int[] collectionSortedArray = collectionsSort.sort(unsortedArray);
         Long collectionSortTime = System.currentTimeMillis() - startTime;
-        return new SortedObject(collectionSortTime, Arrays.stream(collectionSortedArray).boxed().toArray(Integer[]::new));
+        results.put(collectionSortTime, Arrays.stream(collectionSortedArray).boxed().toArray(Integer[]::new));
     }
 
-    private SortedObject getMergeSortResult(int[] unsortedArray) {
+    private void getMergeSortResult(int[] unsortedArray) {
         MergeSort mergeSort = new MergeSort();
         long startTime = System.currentTimeMillis();
         int[] mergeSortedArray = mergeSort.sort(unsortedArray);
         Long mergeSortTime = System.currentTimeMillis() - startTime;
-        return new SortedObject(mergeSortTime, Arrays.stream(mergeSortedArray).boxed().toArray(Integer[]::new));
+        results.put(mergeSortTime, Arrays.stream(mergeSortedArray).boxed().toArray(Integer[]::new));
     }
 
-    private SortedObject getQuickSortResult(int[] unsortedArray) {
+    private void getQuickSortResult(int[] unsortedArray) {
         QuickSort quickSort = new QuickSort();
         long startTime = System.currentTimeMillis();
         int[] quickSortedArray = quickSort.sort(unsortedArray);
         Long quickSortTime = System.currentTimeMillis() - startTime;
-        return new SortedObject(quickSortTime, Arrays.stream(quickSortedArray).boxed().toArray(Integer[]::new));
+        results.put(quickSortTime, Arrays.stream(quickSortedArray).boxed().toArray(Integer[]::new));
     }
 
-    private SortedObject getSelectionSortResult(int[] unsortedArray) {
+    private void getSelectionSortResult(int[] unsortedArray) {
         SelectionSort selectionSort = new SelectionSort();
         long startTime = System.currentTimeMillis();
         int[] selectionSortedArray = selectionSort.sort(unsortedArray);
         Long selectionSortTime = System.currentTimeMillis() - startTime;
-        return new SortedObject(selectionSortTime, Arrays.stream(selectionSortedArray).boxed().toArray(Integer[]::new));
+        results.put(selectionSortTime, Arrays.stream(selectionSortedArray).boxed().toArray(Integer[]::new));
     }
 }
---
+++ src/test/java/io/github/rdx7777/exercisesbackend/service/SortServiceTest.java
@@ -0,0 +1,42 @@
+package io.github.rdx7777.exercisesbackend.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SortServiceTest {
+
+    @InjectMocks
+    SortService sortService;
+
+    @Test
+    void shouldGetAllResults() {
+        // given
+        int[] sortedArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        // when
+        Map<Long, Integer[]> result = sortService.getAllResults(15);
+
+        // then
+        for (Map.Entry<Long, Integer[]> entry : result.entrySet()) {
+            assertThat(entry.getValue()).isEqualTo(sortedArray);
+        }
+    }
+
+    @Test
+    void getAllResultsMethodShouldThrowExceptionForInvalidArguments() {
+        assertThrows(IllegalArgumentException.class, () -> sortService.getAllResults(null),
+            "Array size cannot be null.");
+        assertThrows(IllegalArgumentException.class, () -> sortService.getAllResults(-1),
+            "Array size cannot be lower than zero.");
+        assertThrows(IllegalArgumentException.class, () -> sortService.getAllResults(1_000_001),
+            "Array size out of supported range. The maximum supported array size is 1,000,000.");
+    }
+}
